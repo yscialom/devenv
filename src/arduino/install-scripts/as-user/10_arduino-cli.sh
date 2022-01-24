13332133c1efddd53d747a4778344907a35f9f89
Yankel Scialom (YSC) <yankel@scialom.org>
2022-01-24 23:46:36 +0100

    [INO] Compile & Upload work

--- src/arduino/install-scripts/as-user/10_arduino-cli.sh
+++ src/arduino/install-scripts/as-user/10_arduino-cli.sh
@@ -2,5 +2,16 @@
 set -o pipefail
 
 cat >> ~/.agnostic-shell-aliases <<EOF
-alias arduino-cli="docker run --rm --mount type=bind,source=/home/yscialom/work/.devenv/shell.codedata/arduino,destination=/arduino yankelscialom/arduino-cli-esp8266:0.20.2_0.1.1"
+alias arduino-cli="docker run --rm \
+    --mount type=bind,source=/home/yscialom/work/.devenv/shell.codedata/arduino,destination=/arduino \
+    --device /dev/ttyUSB0 \
+    yankelscialom/arduino-cli-esp8266"
+function arduino-compile () {
+    local sketch="${1:-$(basename $(readlink -f .))}"
+    arduino-cli compile --fqbn esp8266:esp8266:d1 "${sketch}"
+}
+function arduino-upload () {
+    local sketch="${1:-$(basename $(readlink -f .))}"
+    arduino-cli upload --fqbn esp8266:esp8266:d1 --input-dir "/arduino/${sketch}/build/esp8266.esp8266.d1" --port /dev/ttyUSB0
+}
 EOF
