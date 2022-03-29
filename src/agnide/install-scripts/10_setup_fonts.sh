d3dcf037136c2dcca67d2ddc20aa62714eb30877
Yankel Scialom <yankel@scialom.org>
2022-03-29 18:11:54 +0200

    Upgrade to VS Code Server v4.1

--- src/agnide/install-scripts/10_setup_fonts.sh
+++ src/agnide/install-scripts/10_setup_fonts.sh
@@ -2,6 +2,14 @@
 set -x
 set -o pipefail
 
-html=/usr/local/share/.config/yarn/global/node_modules/code-server/src/browser/pages/vscode.html
-font=/ysc/fonts/fonts.html
-sed -i "/<head>/r ${font}" "${html}"
+# hack from @ManiaciaChao, see <https://github.com/coder/code-server/issues/1374#issuecomment-1014203683>
+install_dir=/app/code-server
+mainpage="${install_dir}/vendor/modules/code-oss-dev/out/vs/code/browser/workbench/workbench.html"
+
+sed -i "s,<head>,<head><style>@import url('https://fonts.googleapis.com/css?family=Source+Code+Pro');</style>," "${mainpage}"
+
+grep -rl "style-src 'self' 'unsafe-inline'" "${install_dir}" \
+    | sudo xargs sed -i "s/style-src 'self' 'unsafe-inline'/style-src 'self' 'unsafe-inline' fonts.googleapis.com/g"
+
+grep -rl "font-src 'self' blob:" "${install_dir}" \
+    | sudo xargs sed -i "s/font-src 'self' blob:/font-src 'self' blob: fonts.gstatic.com/g"
