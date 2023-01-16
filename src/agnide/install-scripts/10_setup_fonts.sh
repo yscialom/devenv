3665ddd81fa0921a4f50f233f2bf27858bce3eb4
yscialom <yankel@scialom.org>
2023-01-16 23:16:11 +0100

    test

--- src/agnide/install-scripts/10_setup_fonts.sh
+++ src/agnide/install-scripts/10_setup_fonts.sh
@@ -6,7 +6,7 @@ set -o pipefail
 install_dir=/app/code-server
 mainpage="$(find "${install_dir}" -type f -name workbench.html)"
 
-sed -i "s,<head>,<head><style>\n@font-face {\n  font-family: 'UbuntuMono';\n  font-style: normal;\n  font-weight: 400;\n  src: url(https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v2.2.2/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf) format('truetype');\n}\n</style>," "${mainpage}"
+sed -i "s,<head>,<head><style>\n@font-face {\n  font-family: 'UbuntuMono NFM';\n  font-style: normal;\n  font-weight: 400;\n  src: url(https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v2.2.2/patched-fonts/UbuntuMono/Regular/complete/Ubuntu%20Mono%20Nerd%20Font%20Complete%20Mono%20Windows%20Compatible.ttf) format('truetype');\n}\n</style>," "${mainpage}"
 
 grep -rl "font-src 'self' blob:" "${install_dir}" \
     | sudo xargs sed -i "s/font-src 'self' blob:/font-src 'self' blob: raw.githubusercontent.com/g"
