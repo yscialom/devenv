2f62f3c67b3602abbe600be661ee80ec8bcf8db4
yscialom <yankel@scialom.org>
2025-08-28 00:41:37 +0200

    Install Continue and configure it

--- src/install-scripts/as-user/20_install_extensions.sh
+++ src/install-scripts/as-user/20_install_extensions.sh
@@ -2,4 +2,9 @@
 set -x
 set -o pipefail
 
-/app/code-server/bin/code-server --install-extension samuelcolvin.jinjahtml
+# jinjahtml
+code-server --verbose --install-extension samuelcolvin.jinjahtml
+
+# continue
+code-server --verbose --install-extension continue.continue@1.3.1
+cp /ysc/config/continue-config.yaml /config/.continue/config.yaml
